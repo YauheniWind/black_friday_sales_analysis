f34b3d9bbdbef888b27070b0bb11b02af72b973e
Евгений  Гравдин <evgenijgravdin@MacBook-Pro-Evgenij.local>
2026-06-09 15:07:12 +0200

    Combined uploads DAGs

--- dags/upload_into_source_dag.py
+++ dags/upload_into_source_dag.py
@@ -1,4 +1,3 @@
-import os
 import logging
 from datetime import datetime
 
@@ -6,13 +5,13 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
 
-from scripts.upload_data.upload_data import upload_file_to_s3
+from scripts.upload_data.upload_data import upload_file_to_s3, upload_mongo
 
 logger = logging.getLogger(__name__)
 
 with DAG(
-    dag_id = 'upload_to_s3',
-    description='Upload the data to s3 bucket',
+    dag_id = 'upload_into_sources',
+    description='Upload the data to s3 bucket and mongo db',
     schedule_interval=None,
     start_date=datetime(2026, 5, 30),
     catchup=False,
@@ -25,6 +24,11 @@ with DAG(
         python_callable = upload_file_to_s3
     )
 
+    UPLOAD_DATA_MONGO = PythonOperator(
+        task_id = "UPLOAD_DATA_MONGO",
+        python_callable = upload_mongo
+    )
+
     END = EmptyOperator(task_id = "END")
 
-    START >> UPLOAD_DATA_MINIO >> END
+    START >> UPLOAD_DATA_MINIO >> UPLOAD_DATA_MONGO >> END
