e7c06cd2e25cc6db2f56477788cd338f805d36f3
Евгений  Гравдин <evgenijgravdin@MacBook-Pro-Evgenij.local>
2026-06-10 14:58:48 +0200

    Added trigger

--- dags/upload_into_source_dag.py
+++ dags/upload_into_source_dag.py
@@ -1,13 +1,11 @@
-import logging
 from datetime import datetime
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 from scripts.upload_data.upload_data import upload_file_to_s3, upload_mongo
-
-logger = logging.getLogger(__name__)
 
 with DAG(
     dag_id = 'upload_into_sources',
@@ -29,6 +27,12 @@ with DAG(
         python_callable = upload_mongo
     )
 
+    UPLOAD_INTO_RAW = TriggerDagRunOperator(
+        task_id = "UPLOAD_INTO_RAW",
+        trigger_dag_id = "upload_to_postgres",
+        wait_for_completion = False
+    )
+
     END = EmptyOperator(task_id = "END")
 
-    START >> UPLOAD_DATA_MINIO >> UPLOAD_DATA_MONGO >> END
+    START >> UPLOAD_DATA_MINIO >> UPLOAD_DATA_MONGO >> UPLOAD_INTO_RAW >> END
